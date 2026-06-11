785ce85744ed16e1206a44c4aef18f693b42dd97
Affan Shaikh <iamaffanshaikh71@gmail.com>
2026-06-12 02:28:13 +0530

    minor change in index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res)=>{
 res.send('Hi');
-console.log('/home route hit');
+console.log('/home route hit, hello');
 })
 
 app.listen(8080, '0.0.0.0', ()=>{
