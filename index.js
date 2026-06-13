4a5fb57af881fc625b6766e514b1d0d0676e74f4
Affan Shaikh <iamaffanshaikh71@gmail.com>
2026-06-13 20:03:30 +0530

    /home added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ res.send(`This is Affan from server.`);
 console.log('/home route hit, hello1');
 })
 
+app.get('/home', (req, res)=> {
+    res.send("Hello from home")
+})
+
 app.listen(8080, '0.0.0.0', ()=>{
 console.log("Server is running!");
 })
