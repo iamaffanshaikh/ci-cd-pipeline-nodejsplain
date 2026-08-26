8a6a8a1ef3d0330496aba7f4185db6b35e9c6bfb
Affan Shaikh <iamaffanshaikh71@gmail.com>
2026-08-27 01:29:54 +0530

    checking trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res)=>{
-res.send(`This is Affan from sfgdfgfd.`);
+res.send(`This is Affan.`);
 console.log('/home route hit, hello1');
 })
 
